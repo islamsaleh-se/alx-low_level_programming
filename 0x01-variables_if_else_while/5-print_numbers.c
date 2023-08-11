df840a1a590170f03f316ace92cd44e51f35121a
ISlam <islamsaleh.se@gmail.com>
2023-08-12 00:10:29 +0300

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+	*
+ * Description: prints all single digit numbers of base 10 starting from 0.
+	*
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	return (0);
+}
